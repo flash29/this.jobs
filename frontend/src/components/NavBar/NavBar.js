f78fddc99be15e9be56ccf86e2f2b70a7fc2ab32
flash29 <ranjeetmallipeddi9@gmail.com>
2022-01-22 14:54:27 -0500

    Developed a navigation bar with the title "this.jobs".

---
+++ frontend/src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <div>
+     <div className="NavBar">
+       <div className="title">
+          this.jobs
+       </div>
+       <div className="Search Bar">
+          Search Bar will be here
+       </div>
+       <div className="iconsHolder">
+          Icons will be placed here
+       </div>
+      
+     </div>
+    </div>
+  );
+}
+
+export default NavBar;
