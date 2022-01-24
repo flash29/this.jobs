73995e4982936dea2fdb12dec4c25dc6c0b54750
SnehaParuchuri <sneha.paruchuri96@gmail.com>
2022-01-24 00:36:29 -0500

    Tested GET and POST REST API using gin framework

---
+++ backend/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//feed model
+type userPost struct {
+	ID        string   `json:"id"`
+	CreatedBy string   `json:"createdBy"`
+	Content   string   `json:"content"`
+	Likes     float64  `json:"likes"`
+	Comments  []string `json:"comments"`
+}
+
+var feed = []userPost{
+	{ID: "1", CreatedBy: "Sneha", Content: "Retrieved", Likes: 10, Comments: nil},
+}
+
+func getFeed(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, feed)
+}
+
+// createPost adds a post from JSON received in the request body.
+func createPost(c *gin.Context) {
+	var newPost userPost
+
+	// Call BindJSON to bind the received JSON to
+	// newPost.
+	if err := c.BindJSON(&newPost); err != nil {
+		return
+	}
+
+	// Add the new post to the slice.
+	feed = append(feed, newPost)
+	c.IndentedJSON(http.StatusCreated, newPost)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/feed", getFeed)
+	router.POST("/createPost", createPost)
+
+	router.Run("localhost:8080")
+}
