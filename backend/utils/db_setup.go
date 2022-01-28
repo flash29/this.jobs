8cc47c34c6e40f6cdb6e7cd12a3a5013ad7bc8c9
VishnuJammula <211vishnu@gmail.com>
2022-01-28 02:10:26 -0500

    GORM configuration and entity refactoring

---
+++ backend/utils/db_setup.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"com.uf/src/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	database, err := gorm.Open("sqlite3", "jobs.db")
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	database.AutoMigrate(&models.UserPost{})
+
+	DB = database
+}
