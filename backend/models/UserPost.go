71e575622e942dae7112866f210fee7a1ddd55ec
SnehaParuchuri <sneha.paruchuri96@gmail.com>
2022-01-29 13:31:44 -0500

    updated userpost model

--- backend/models/UserPost.go
+++ backend/models/UserPost.go
@@ -1,8 +1,19 @@
 package models
 
+import "github.com/jinzhu/gorm"
+
 type UserPost struct {
-	ID        string  `json:"id"`
-	CreatedBy string  `json:"createdBy"`
-	Content   string  `json:"content"`
-	Likes     float64 `json:"likes"`
+	// ID        string    `json:"id,primary_key"`
+
+	gorm.Model
+	CreatedBy   string    `json:"createdBy"`
+	Content     string    `json:"content"`
+	Likes       []string  `json:"likes"`
+	Comments    []Comment `json:"comments"`
+	Attachments []string  `json:"attachemnts"`
+}
+
+type Comment struct {
+	gorm.Model
+	Comment string `json:"comment"`
 }
