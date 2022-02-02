24d6cba66e9bec21cbe64f96398b365122f8dd74
vangmayi98 <vangmayi98@gmail.com>
2022-02-02 11:56:14 -0500

    created a route to the searchbar

---
+++ frontend/src/dashboard/Search/Search.js
@@ -0,0 +1,12 @@
+import NavBar from '../../components/NavBar/NavBar'
+
+function Search() {
+    return (
+      <div className="App">
+          <NavBar />
+          Search page under construction!
+      </div>
+    );
+  }
+  
+  export default Search;
