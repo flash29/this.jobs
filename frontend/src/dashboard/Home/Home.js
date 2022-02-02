87f276f3d152e0f22e7a06b565389d9439487084
flash29 <ranjeetmallipeddi9@gmail.com>
2022-02-02 02:21:02 -0500

    gets the user posts from the server to be displayed on the feed and sends them to the props PostCard and Card to display the posts to the user

--- frontend/src/dashboard/Home/Home.js
+++ frontend/src/dashboard/Home/Home.js
@@ -1,10 +1,29 @@
 import NavBar from '../../components/NavBar/NavBar'
+import PostCard from './PostCard/PostCard';
+import React, { useState, useEffect } from 'react';
+
+
 
 function Home() {
+
+  const [posts, setPosts] = useState();
+
+  async function getPosts () {
+      fetch('/feed')
+        .then(res => res.json())
+        .then(res => setPosts(res) )
+        .catch(error => console.log('error getting Feed'))
+  }
+  useEffect(() => {
+    getPosts();
+    console.log('posts', posts);
+  }, []);
+
+
   return (
     <div className="App">
       <NavBar />
-      You are in home page
+      <PostCard posts={posts}/>
     </div>
   );
 }
