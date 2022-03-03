a71b3bfdceaa7140d322be80cae0f97f7c094fab
VishnuJammula <211vishnu@gmail.com>
2022-03-03 15:51:56 -0500

    Revert "setup for profile controller"
    This reverts commit ca679fc2fe04d2e217ada9b2ba85571f61640c1c.
    
    Reverting because of wrong branch`

--- backend/models/User.go
+++ backend/models/User.go
@@ -9,32 +9,5 @@ type User struct {
 	UserName       string         `gorm:"type:text" json:"userName"`
 	Password       string         `gorm:"type:text" json:"password"`
 	DisplayPicture string         `gorm:"type:text" json:"dp"`
-	Bio            string         `gorm:"type:text" json:"bio"`
 	Following      pq.StringArray `gorm:"type:text[]" json:"following"`
-	EducationList  []Education    `gorm:"ForeignKey:UserID" json:"education"`
-	PorjectList    []Project      `gorm:"ForeignKey:UserID" json:"projects"`
-	JobHistoryList []JobHistory   `gorm:"ForeignKey:UserID" json:"jobhistory"`
-}
-
-type Education struct {
-	EducationId int    `gorm:"primary_key" json:"educationId"`
-	InsName     string `json:"InsName"`
-	Timeline    string `json:"timeline"`
-	Gpa         string `json:"gpa"`
-	UserID      uint   `json:"userId"`
-}
-
-type Project struct {
-	ProjectId   int    `gorm:"primary_key" json:"projectId"`
-	ProjName    string `json:"projName"`
-	Description string `json:"description"`
-	UserID      uint   `json:"userId"`
-}
-
-type JobHistory struct {
-	JobHistoryId int    `gorm:"primary_key" json:"jobHistoryId"`
-	Company      string `json:"company"`
-	Timeline     string `json:"timeline"`
-	Position     string `json:"position"`
-	UserID       uint   `json:"userId"`
 }
--- backend/utils/db_setup.go
+++ backend/utils/db_setup.go
@@ -16,11 +16,8 @@ func ConnectDatabase() {
 	}
 
 	//database.Model(&models.UserPost{}).Related(&models.Comment{})
-	database.AutoMigrate(models.User{}, models.Education{}, models.JobHistory{}, models.Project{}, models.UserPost{}, models.Comment{})
+	database.AutoMigrate(models.UserPost{}, models.Comment{})
 	database.Model(&models.Comment{}).AddForeignKey("post_id", "user_posts(id)", "CASCADE", "CASCADE")
-	database.Model(&models.Education{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
-	database.Model(&models.Project{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
-	database.Model(&models.JobHistory{}).AddForeignKey("user_id", "users(id)", "CASCADE", "CASCADE")
 	//database.AutoMigrate(models.Comment{})
 
 	DB = database
