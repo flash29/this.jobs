269422010dd2e0f2fcda3da1417d8096a121bad8
SnehaParuchuri <sneha.paruchuri96@gmail.com>
2022-04-09 17:32:47 -0400

    updated connection-request model

--- backend/models/ConnectionRequest.go
+++ backend/models/ConnectionRequest.go
@@ -1,9 +1,8 @@
 package models
 
 type ConnectionRequest struct {
-	RequestID   int   `gorm:"primary_key" json:"requestId"`
-	RequestedBy int   `json:"requestedBy"`
-	ConnectTo   int   `json:"connectTo"`
-	CreatedAt   int64 `json:"createdAt"`
-	UpdatedAt   int64 `json:"updatedAt"`
+	RequestID     int   `gorm:"primary_key" json:"requestId"`
+	RequestedFrom int   `json:"requestedFrom"`
+	RequestedTo   int   `json:"requestedTo"`
+	CreatedAt     int64 `json:"createdAt"`
 }
