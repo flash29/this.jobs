686ff410089b76cace0c011dfed96bf613913eb2
SnehaParuchuri <sneha.paruchuri96@gmail.com>
2022-04-10 17:28:15 -0400

    fixes #140 retrieve conenction requests

--- backend/models/ConnectionRequest.go
+++ backend/models/ConnectionRequest.go
@@ -1,8 +1,9 @@
 package models
 
 type ConnectionRequest struct {
-	RequestID     int   `gorm:"primary_key" json:"requestId"`
-	RequestedFrom int   `json:"requestedFrom"`
-	RequestedTo   int   `json:"requestedTo"`
-	CreatedAt     int64 `json:"createdAt"`
+	RequestID     int    `gorm:"primary_key" json:"requestId"`
+	RequestedFrom int    `json:"requestedFrom"`
+	RequestorName string `json:"requestorName"`
+	RequestedTo   int    `json:"requestedTo"`
+	CreatedAt     int64  `json:"createdAt"`
 }
