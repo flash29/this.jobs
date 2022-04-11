89b104de697ce88dc72989ca9ef521e7488fd594
VishnuJammula <211vishnu@gmail.com>
2022-04-11 16:39:04 -0400

    #142: people search by education, job history, projects

--- backend/controllers/search-controller.go
+++ backend/controllers/search-controller.go
@@ -8,6 +8,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type SearchResult struct {
+	UserID    int    `json:"userId"`
+	UserEmail string `json:"useremail" `
+	UserName  string `json:"username"`
+	Bio       string `json:"bio"`
+}
+
 func SearchPeople(c *gin.Context) {
 	searchTerm, present := c.GetQuery("search")
 
@@ -15,16 +22,30 @@ func SearchPeople(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Search query not present"})
 		return
 	}
-	var searchRes []models.User
+	var dbRes []models.User
+	var searches []SearchResult
 	likeSearchTerm := "%" + searchTerm + "%"
+	selectString := `users.user_id, users.user_email, users.user_name, users.bio`
+
+	likeString := `users.user_email like ? or users.user_name like ? or users.bio like ? or 
+                   educations.ins_name like ? or job_histories.company like ? or job_histories.position like ?
+                   or projects.proj_name like ? or projects.description like ?`
+
 	results := utils.
-		DB.Select("user_id, user_email,user_name,display_picture,bio").
-		Find(&searchRes, "user_email like ? or user_name like ? or bio like ?", likeSearchTerm, likeSearchTerm, likeSearchTerm)
+		DB.
+		Select(selectString).
+		Joins("left join educations on educations.user_id = users.user_id").
+		Joins("left join job_histories on job_histories.user_id = users.user_id").
+		Joins("left join projects on projects.user_id = users.user_id").
+		Where(likeString, likeSearchTerm, likeSearchTerm, likeSearchTerm, likeSearchTerm, likeSearchTerm, likeSearchTerm, likeSearchTerm, likeSearchTerm).
+		Group("users.user_id").
+		Find(&dbRes).
+		Scan(&searches)
 
 	if results.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to retrieve results"})
 		return
 	}
 
-	c.JSON(http.StatusOK, searchRes)
+	c.JSON(http.StatusOK, searches)
 }
