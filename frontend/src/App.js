2ca0e0bc7366ce5c95e673d1c492d49a72f72599
flash29 <ranjeetmallipeddi9@gmail.com>
2022-04-11 14:02:45 -0400

    Added Protected Route User now cant access routes that he is not allowed to. When user is not logged in he cant access the home page and other dashboards.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,27 +10,132 @@ import ErrorPage from './ErrorPage/ErrorPage'
 import {BrowserRouter , Routes, Route} from "react-router-dom";
 import LoginPage from './dashboard/LoginPage/LoginPage';
 import Registration from './dashboard/Registration/Registration';
+import ProtectedRoute from './auth/ProtectedRoute';
 
+import { useState, useContext } from 'react';
+import React from 'react';
 
+const AuthContext = React.createContext(null);
+
+const getAuth = ({postData}) => {
+
+  fetch('/auth/login', {
+      method : 'POST',
+      headers:{'Content-type':'application/json'},
+      //headers:{'Authorization' : 'Bearer '}
+      body:JSON.stringify(postData),
+  }).then(response => response.json()).then(data => {
+      console.log(data);
+      sessionStorage.setItem("token", data.token);
+      sessionStorage.setItem(data.userEmail, data.password);
+      sessionStorage.setItem('userid', data.userId);
+      sessionStorage.setItem('token', data.token);
+      console.log('stored token', sessionStorage.getItem('token') );
+      console.log('userid stored', sessionStorage.getItem('userid')   );
+    //  setPostData({ useremail : '', password : ''});
+      //window.location.reload(false)
+     // let path = "/home";
+   //   navigate(path);
+  }).catch(error => console.log('error', error))
+} 
+
+const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState(null);
+  
+    const handleLogin = async () => {
+        getAuth();
+  
+      setToken(sessionStorage.getItem('token'));
+    };
+  
+    const handleLogout = () => {
+      setToken(null);
+    };
+  
+    const value = {
+      token,
+      onLogin: handleLogin,
+      onLogout: handleLogout,
+    };
+  
+    return (
+      <AuthContext.Provider value={value}>
+        {children}
+      </AuthContext.Provider>
+    );
+  };
+
+const useAuth = () => {
+    return useContext(AuthContext);
+};
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path = "/search" element = {<Search />} />
-        <Route path="/connections" element={<Connections />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/settings/profile/:userid" element={<MyProfile />} />
-        <Route path="/settings/changepassword" element={<ChangePassword />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+      <AuthProvider>
+        <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path = "/search" 
+            element = {
+              <ProtectedRoute>
+                  <Search />
+              </ProtectedRoute>
+            
+          } 
+            />
+            <Route path="/connections" 
+            element={
+              <ProtectedRoute>
+                <Connections />
+              </ProtectedRoute>
+          } 
+            />
+            <Route path="/jobs" element={
+              <ProtectedRoute>
+                 <Jobs />
+              </ProtectedRoute>
+           
+            } />
+            <Route path="/settings" 
+            element={
+              <ProtectedRoute>
+                <Settings />
+              </ProtectedRoute>
+            
+          } 
+            />
+            <Route path="/settings/profile/:userid" 
+            element={
+              <ProtectedRoute>
+                <MyProfile />
+              </ProtectedRoute>
+            
+          } 
+            />
+            <Route path="/settings/changepassword" 
+            element={
+              <ProtectedRoute>
+                <ChangePassword />
+              </ProtectedRoute>
+            
+            } />
+            {/* <Route path="/login" element={<Login />} /> */}
 
-        <Route path="/home" element={<Home />} />
-        <Route path = "/registration" element = {<Registration />} />
+            <Route path="/home" 
+                  element=
+                  {
+                    <ProtectedRoute>
+                        <Home />
+                    </ProtectedRoute>
+                    
+                  } 
+            />
+            <Route path = "/registration" element = {<Registration />} />
 
-        <Route path="*" element={<ErrorPage />} />
-      </Routes> 
+            <Route path="*" element={<ErrorPage />} />
+          </Routes> 
+      </AuthProvider>
+        
     </BrowserRouter>
   );
 }
