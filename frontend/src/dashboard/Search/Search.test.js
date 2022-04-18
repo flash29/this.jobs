87f6483fcd9218a8f21c33e146841aff1e7263a1
vangmayi98 <vangmayi98@gmail.com>
2022-04-18 19:08:12 -0400

    Added more Unit Tests

--- frontend/src/dashboard/Search/Search.test.js
+++ frontend/src/dashboard/Search/Search.test.js
@@ -3,6 +3,8 @@ import { Router, BrowserRouter } from 'react-router-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
 import {createMemoryHistory} from 'history'
 import Search from './Search';
+import PeopleSearchCard from './PeopleSearchCard';
+import JobsSearchCard from './JobsSearchCard';
 
 test("NavBar is rendering", () => {
     const {container} = render(
@@ -43,3 +45,39 @@ test("Displaying all Search Components", () => {
         </BrowserRouter>);
     expect(container.classList.contains("cardDisplay"));    
 });
+
+//People Search Card - 
+
+test("Displaying the user's name in PeopleSearch Card", () => {
+    const {container} = render(
+        <BrowserRouter>
+           <PeopleSearchCard />
+        </BrowserRouter>);
+    expect(container.classList.contains("searchJobTitle"));    
+});
+
+test("Displaying the user's email in PeopleSearch Card", () => {
+    const {container} = render(
+        <BrowserRouter>
+           <PeopleSearchCard />
+        </BrowserRouter>);
+    expect(container.classList.contains("searchOrg"));    
+});
+
+test("Displaying the user's bio in PeopleSearch Card", () => {
+    const {container} = render(
+        <BrowserRouter>
+           <PeopleSearchCard />
+        </BrowserRouter>);
+    expect(container.classList.contains("searchlocation"));    
+});
+
+// JobSearch Card 
+test("Displaying all the elements of JobSearchCard", () => {
+    const {container} = render(
+        <BrowserRouter>
+           <JobsSearchCard />
+        </BrowserRouter>);
+    expect(container.classList.contains("jobCard"));    
+});
+
