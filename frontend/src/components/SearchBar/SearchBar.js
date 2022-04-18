af02853865fb127c98d95d50b3f1fe7d35bc2c91
flash29 <ranjeetmallipeddi9@gmail.com>
2022-04-18 18:19:51 -0400

    when user types in the text field and presses the enter keyword it redirects to the search page with query params

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,15 +1,52 @@
 //import logo from './logo.svg';
 import "./SearchBar.css";
-import sicon from "../../images/search-outline.svg";
-import {Link} from "react-router-dom";
+import {useState} from 'react';
+import {
+    useNavigate,
+    createSearchParams,
+  } from 'react-router-dom';
 
 function SearchBar(){
+    const navigate = useNavigate();
+
+    const[searchCheck, setSearchCheck] = useState('');
+
+
+
+    const handleChange = (e) =>{
+        setSearchCheck(e.target.value)
+        // console.log('value is ', e.target.value)
+    }
+
+    const handleSearch = () => {
+        console.log('the search value is ', searchCheck);
+        navigate({
+            pathname: '/search',
+            search: `?${createSearchParams({ query: searchCheck })}`,
+          });
+    }
+
+    const handleEnter = (e) => {
+        // console.log('hi', e)
+        let code = e.keyCode || e.which;
+        if ( code === 13 ) {
+            // console.log('Enter pressed')
+            handleSearch();
+          }
+    }
+
+
     return( 
-        <div className = "searchBar">
-            <input type = "text" placeholder = "Search"/>
-            <Link to = "/search">
-                <img src = {sicon} className = "SearchIcon"/>
-            </Link>
+        <div className = "searchBarDisplay">
+            <input 
+            type = "text" 
+            placeholder = "Search" 
+            className="searchInput" 
+            value= {searchCheck}
+            onChange={handleChange}
+            onKeyPress = {handleEnter}
+            id = "searchInput"
+            />
         </div>
     );
 }
